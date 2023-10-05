59ee40daf8e9e36eff9b05c4e13bcbcffead3509
Andre Medina <andre.medinav@hotmail.com>
2023-10-05 14:32:57 -0300

    mudanca final
    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [tarefa, setTarefa] = useState([]);
@@ -24,40 +24,54 @@ function App() {
     setTarefa(tarefaAtualizada);
   };
 
-
-
-
   return (
-    <div>
-      <h1 class="title">Lista de Tarefas:</h1>
+    <div class="container">
+      <div class="container__title">
+        <img class="icone" src="./Porter.png" />
+        <h1 class="title">Lista de Tarefas:</h1>
+      </div>
       <div class="input">
-        <input class="tarefa"
+        <input
+          class="tarefa"
           type="text"
           placeholder="Digite uma nova tarefa"
           value={novaTarefa}
           onChange={(e) => setNovaTarefa(e.target.value)}
         />
-        <button class="botao" onClick={handleAdicionarTarefa}>Adicionar</button>
+        <button class="botao" onClick={handleAdicionarTarefa}>
+          Adicionar
+        </button>
       </div>
-      <ul class="input__on">
-        {tarefa.map((task, index) => (
-          <li key={index}>
-            {task.completed && <span>&#10003;</span>}
-            <span
-              style={{
-                textDecoration: task.completed,
-                color: task.completed ? "green" : "red",
-              }}
-            >
-              {task.name}
-            </span>
-            <button class="botao__concluir" onClick={() => handleAlternarConcluido(index)}>
-              {task.completed ? "Desfazer" : "Concluir"}
-            </button>
-            <button class="botao__remover" onClick={() => handleRemoverTarefa(index)}>Remover</button>
-          </li>
-        ))}
-      </ul>
+      <div class="section">
+        <ul class="input__on">
+          {tarefa.map((task, index) => (
+            <li key={index}>
+              <input
+                class="checkbox"
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => handleAlternarConcluido(index)}
+              />
+              {task.completed && <span>&#10003;</span>}
+              <span
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                  color: task.completed ? "green" : "black",
+                  opacity: task.completed ? 0.3 : 1,
+                }}
+              >
+                {task.name}
+              </span>
+              <button
+                class="remover"
+                onClick={() => handleRemoverTarefa(index)}
+              >
+                x
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
